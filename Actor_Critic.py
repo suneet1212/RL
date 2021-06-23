387f76da96f1ccada19a2e507ce81957d639f9a0
suneet1212 <suneets2000@gmail.com>
2021-06-23 11:31:24 +0530

    Actor-Critic added

--- Actor_Critic.py
+++ Actor_Critic.py
@@ -1,67 +1,138 @@
 # Environment is Lunar Lander
 from gym import spaces
 from gym.spaces import space
-from Deep_Q_Network import NUM_EPISODES
+from torch.nn.modules.activation import ReLU, Softmax
 import gym
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.distributions.multivariate_normal as mnormal
+import torch.distributions.categorical as c
 from collections import deque
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 ###############################################
-LR = 0.01
+LR = 0.1
 NUM_EPISODES = 10000
 GAMMA = 0.99
 ###############################################
 
-class Actor_Critic(nn.Module):
+# class Actor(nn.Module):
+#     def __init__(self):
+#         super(Actor,self).__init__()
+#         self.linear_stack = nn.Sequential(
+#             nn.Linear(8,10),
+#             nn.ReLU(),
+#             nn.Linear(10,10),
+#             nn.ReLU(),
+#             nn.Linear(10,4),
+#             nn.Softmax()
+#         )
+#     def forward(self, x):
+#         x = self.linear_stack(x)
+#         return x
+
+# class Critic(nn.Module):
+#     def __init__(self):
+#         super(Critic, self).__init__()
+#         self.linear_stack = nn.Sequential(
+#             nn.Linear(8,10),
+#             nn.ReLU(),
+#             nn.Linear(10,10),
+#             nn.ReLU(),
+#             nn.Linear(10,4)
+#         )
+
+#     def forward(self, x):
+#         x = self.linear_stack(x)
+#         return x
+
+class Actor_critic(nn.Module):
     def __init__(self):
-        super(Actor_Critic,self).__init__()
-        self.linear_stack = nn.Sequential(
-            nn.Linear(24,50),
+        super(Actor_critic,self).__init__()
+        self.actor_linear_stack = nn.Sequential(
+            nn.Linear(8,50),
+            nn.ReLU(),
+            nn.Linear(50,50),
+            nn.ReLU(),
+            nn.Linear(50,50),
+            nn.ReLU(),
+            nn.Linear(50,50),
             nn.ReLU(),
             nn.Linear(50,50),
             nn.ReLU(),
             nn.Linear(50,4),
             nn.Softmax()
         )
-    def forward(self, x):
-        x = self.linear_stack(x)
-        return x
+        self.critic_linear_stack = nn.Sequential(
+            nn.Linear(8,60),
+            nn.ReLU(),
+            nn.Linear(60,10),
+            nn.ReLU(),
+            nn.Linear(10,4)
+        )
+    def forward(self,x):
+        actor = self.actor_linear_stack(x)
+        critic = self.critic_linear_stack(x)
+        return actor, critic
 
 class Agent:
     def __init__(self) -> None:
-        self.model = Actor_Critic().to(device)
+        # self.actor = Actor().to(device)
+        # self.critic = Critic().to(device)
+        self.model = Actor_critic().to(device)
         self.env = gym.make('LunarLander-v2')
         self.state = self.env.reset()
-        self.episode = []
+        # self.optimizer_actor = torch.optim.Adam(self.actor.parameters(), lr=LR)
+        # self.optimizer_critic = torch.optim.Adam(self.critic.parameters(), lr=LR)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=LR)
-        self.G = []
-        self.grads = []
-        # self.loss = None
         self.total_rewards = 0
+    
+    def train_loop(self):
+        probs, q = (self.model((torch.from_numpy(self.state).unsqueeze(0).float().to(device))))
+        probs = torch.squeeze(probs)
+        m = c.Categorical(probs)
+        action = m.sample()
+        next_state, reward, done, _ = self.env.step(action.item())
+        self.env.render()
+        probs2, q_prime = self.model(torch.from_numpy(next_state).unsqueeze(0).float().to(device))
+        probs2 = torch.squeeze(probs2)
+        # q = self.critic(torch.from_numpy(self.state).unsqueeze(0).float().to(device))
+        # probs2 = (self.actor((torch.from_numpy(next_state).unsqueeze(0)).float().to(device)))
+        m2 = c.Categorical(probs2)
+        action2 = m2.sample().item()
+        if done == True:
+            q_prime[0][action2] = 0
+        td_error = reward + GAMMA*q_prime[0][action2] - q[0][action]
+        loss_a = -m.log_prob(action) * td_error
+        loss_obj = nn.MSELoss()
+        loss_c = -loss_obj(torch.tensor([reward], device=device).float() + GAMMA*q_prime[0][action2].float(), q[0][action].float())
+        loss = loss_a + loss_c
+        # self.optimizer_actor.zero_grad()
+        # self.optimizer_critic.zero_grad()
+        self.optimizer.zero_grad()
+        # loss_a.backward()
+        # loss_c.backward()
+        loss.backward()
+        # self.optimizer_actor.step()
+        # self.optimizer_critic.step()
+        self.optimizer.step()
 
-    # def 
-
+        # update next state
+        if done == True:
+            self.state = self.env.reset()
+        else:
+            self.state = next_state
+        return done, reward  
 
 if __name__ == "__main__":
     agent = Agent()
-    deq = deque(maxlen = 100)
-    scores = []
+    # deq = deque(maxlen = 100)
+    # scores = []
     for i in range(NUM_EPISODES):
-        total_reward = agent.train_loop()
-        print(f"Number of Episodes : {i+1}   Total reward = {total_reward}" )
-        deq.append(total_reward)
-        scores.append(total_reward)
-        if np.mean(deq)>200:
-            print(f'\nEnvironment solved in {i-100} episodes!\tAverage Score: {np.mean(deq):.2f}')
-            torch.save(agent.model.state_dict(), 'actor_critic_lunar_lander.pth')
-            break
-    
-    x = input()
-    if x == 'y':
-        torch.save(agent.model.state_dict(), 'actor_critic_lunar_lander.pth')
-    
+        done = False
+        total_reward = 0
+        while not done:
+            done, reward = agent.train_loop()
+            total_reward += reward
+        print(f"Number of Episodes : {i+1}   Total reward = {total_reward}")    
